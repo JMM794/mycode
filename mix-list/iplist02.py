07e0764edb0346c1b1d8459de4a667e1b479c771
JMM794 <justinmerchant14@gmail.com>
2024-03-27 20:06:27 +0000

    copying local files with python

---
+++ mix-list/iplist02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("IP address: " + iplist [3] + ", and " + iplist[4])
+print("IP address:", iplist[3] + ", and " + iplist[4]) 
+print (f"IP address: {iplist[3]}, and {iplist[4]}")
+
