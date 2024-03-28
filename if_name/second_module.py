9e5f055a832e6fb175071c0a5ec9d72bd98969bb
JMM794 <justinmerchant14@gmail.com>
2024-03-28 20:59:12 +0000

    A bit on error handling

---
+++ if_name/second_module.py
@@ -0,0 +1,12 @@
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+
+def main():
+    print("This code belongs to the main fuction in module 1.")
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ iftest/condition01.py
@@ -0,0 +1,14 @@
+hostname = "MTG"
+
+hostname = input("What value should we set for hostname?")
+
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print()
+    print("The hostname matches expected config")
+    print()
+else: 
+    print("Did not find hostname")
+
+print("Exiting the script")
+
